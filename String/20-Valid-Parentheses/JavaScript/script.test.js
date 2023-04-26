aef12ff5f53975fcc9bb5aefe5cf176081fe876f
Luke Lai <lukelai.dev@gmail.com>
2023-04-26 21:05:29 +0100

    Add test for isValid to return false when given wrong pair of brackets

--- String/20-Valid-Parentheses/JavaScript/script.test.js
+++ String/20-Valid-Parentheses/JavaScript/script.test.js
@@ -16,4 +16,9 @@ describe("isValid", () => {
     expect(isValid("()[]")).toEqual(true);
     expect(isValid("[]{}()")).toEqual(true);
   });
+
+  it("returns false given wrong closing type and more than one pair of brackets ", () => {
+    expect(isValid("(][)")).toEqual(false);
+    expect(isValid("[}()")).toEqual(false);
+  });
 });
