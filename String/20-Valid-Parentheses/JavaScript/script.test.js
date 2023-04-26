c6077605ce34ab191aa4f824a146f29baadc8383
Luke Lai <lukelai.dev@gmail.com>
2023-04-26 20:50:59 +0100

    Add a test for single wrong type returning false

--- String/20-Valid-Parentheses/JavaScript/script.test.js
+++ String/20-Valid-Parentheses/JavaScript/script.test.js
@@ -5,6 +5,10 @@ describe("isValid", () => {
     expect(isValid("()")).toEqual(true);
     expect(isValid("[]")).toEqual(true);
     expect(isValid("{}")).toEqual(true);
+  });
+
+  it("returns false given wrong type", () => {
     expect(isValid("(}")).toEqual(false);
+    expect(isValid("[}")).toEqual(false);
   });
 });
