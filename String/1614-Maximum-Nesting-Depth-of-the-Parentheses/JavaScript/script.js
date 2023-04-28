87c43c55773882157d61196d5d7bf4499ce0ef82
Luke Lai <lukelai.dev@gmail.com>
2023-04-28 22:46:57 +0100

    Add test to return the count of deepest nested paretheses when given a math expression

--- String/1614-Maximum-Nesting-Depth-of-the-Parentheses/JavaScript/script.js
+++ String/1614-Maximum-Nesting-Depth-of-the-Parentheses/JavaScript/script.js
@@ -1,5 +1,21 @@
 var maxDepth = function (s) {
-  return true;
+  let left = 0;
+  let right = 0;
+  let count = 0;
+  for (let index = 0; index < s.length; index++) {
+    if (s[index] === "(") {
+      left++;
+    } else if (s[index] === ")") {
+      right++;
+    }
+
+    if (left === right) {
+      count = left;
+      left = 0;
+      right = 0;
+    }
+  }
+  return count;
 };
 
 module.exports = maxDepth;
--- String/1614-Maximum-Nesting-Depth-of-the-Parentheses/JavaScript/script.test.js
+++ String/1614-Maximum-Nesting-Depth-of-the-Parentheses/JavaScript/script.test.js
@@ -1,7 +1,8 @@
 const maxDepth = require("./script");
 
 describe("maxDepth", () => {
-  it("returns true", () => {
-    expect(maxDepth()).toEqual(true);
+  it("returns the number of deepest nested paretheses", () => {
+    expect(maxDepth("(1)+((2))+(((3)))")).toEqual(3);
+    expect(maxDepth("(1)+((2))+((3))")).toEqual(2);
   });
 });
