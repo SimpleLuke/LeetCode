cbc0f337b2f230c0b1e7d065e02fcc5069fbb53b
Luke Lai <lukelai.dev@gmail.com>
2023-04-29 22:08:09 +0100

    Add one more test case to check the test

--- String/205-Isomorphic-Strings/script.test.js
+++ String/205-Isomorphic-Strings/script.test.js
@@ -3,5 +3,6 @@ const isIsomorphic = require("./script");
 describe("isIsomorphic", () => {
   it("returns true when given the characters of first word can be replaced to get second word", () => {
     expect(isIsomorphic("egg", "add")).toEqual(true);
+    expect(isIsomorphic("paper", "title")).toEqual(true);
   });
 });
