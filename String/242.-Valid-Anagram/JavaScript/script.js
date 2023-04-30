461a504bde16915311e53ae2b46dd86648c9c264
Luke Lai <lukelai.dev@gmail.com>
2023-04-30 22:14:16 +0100

    Add tests to return true when given words are anagram and return false when given words are not anagram. And pass the test

--- String/242.-Valid-Anagram/JavaScript/script.js
+++ String/242.-Valid-Anagram/JavaScript/script.js
@@ -1,5 +1,19 @@
 var isAnagram = function (s, t) {
-  return true;
+  if (s.length !== t.length) {
+    return false;
+  }
+  const tArray = t.split("");
+  for (let index = 0; index < s.length; index++) {
+    let i = tArray.indexOf(s[index]);
+    if (i > -1) {
+      tArray.splice(i, 1);
+    }
+  }
+  if (tArray.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = isAnagram;
--- String/242.-Valid-Anagram/JavaScript/script.test.js
+++ String/242.-Valid-Anagram/JavaScript/script.test.js
@@ -1,7 +1,14 @@
 const isAnagram = require("./script");
 
 describe("isAnagram", () => {
-  it("returns true", () => {
-    expect(isAnagram()).toEqual(true);
+  it("returns true given two words are anagram", () => {
+    expect(isAnagram("anagram", "nagaram")).toEqual(true);
+    expect(isAnagram("ten", "net")).toEqual(true);
+  });
+  it("returns false given two words are not anagram", () => {
+    expect(isAnagram("rat", "cat")).toEqual(false);
+  });
+  it("returns false given different word length", () => {
+    expect(isAnagram("rat", "at")).toEqual(false);
   });
 });
