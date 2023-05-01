211f1b6bc7e7a584c9cc19065eb689b91ea1711a
Luke Lai <lukelai.dev@gmail.com>
2023-05-01 21:50:07 +0100

    Add an O(n) approach with one loop and conditions to aviod long string reaching excution time limit

--- String/859-Buddy-Strings/JavaScript/script.js
+++ String/859-Buddy-Strings/JavaScript/script.js
@@ -1,20 +1,51 @@
+// This approach will lead to Time Limit Exceeded with long string
+// var buddyStrings = function (s, goal) {
+//   let array = s.split("");
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//       if (i === j) {
+//         continue;
+//       }
+//       let temp = array[j];
+//       let tempArr = [...array];
+//       tempArr[j] = tempArr[i];
+//       tempArr[i] = temp;
+//       console.log(tempArr.join(""));
+//       if (tempArr.join("") === goal) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// };
+
+// O(n)
 var buddyStrings = function (s, goal) {
-  let array = s.split("");
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if (i === j) {
-        continue;
-      }
-      let temp = array[j];
-      let tempArr = [...array];
-      tempArr[j] = tempArr[i];
-      tempArr[i] = temp;
-      console.log(tempArr.join(""));
-      if (tempArr.join("") === goal) {
-        return true;
-      }
+  if (s.length !== goal.length) {
+    return false;
+  }
+
+  if (s === goal) {
+    if (s.length !== new Set(s).size) {
+      return true;
+    }
+    return false;
+  }
+
+  let diff = [];
+  for (let index = 0; index < s.length; index++) {
+    if (s[index] !== goal[index]) {
+      diff.push(index);
     }
   }
+
+  if (diff.length > 2) {
+    return false;
+  }
+  if (s[diff[0]] === goal[diff[1]] && s[diff[1]] === goal[diff[0]]) {
+    return true;
+  }
+
   return false;
 };
 
--- String/859-Buddy-Strings/JavaScript/script.test.js
+++ String/859-Buddy-Strings/JavaScript/script.test.js
@@ -3,9 +3,11 @@ const buddyStrings = require("./script");
 describe("isAnagram", () => {
   it("returns true when given first input can become second input after switching two indices", () => {
     expect(buddyStrings("ab", "ba")).toEqual(true);
+    expect(buddyStrings("abab", "abab")).toEqual(true);
     expect(buddyStrings("abcde", "cbade")).toEqual(true);
   });
   it("returns false when given first input cannot become second input after switching two indices", () => {
     expect(buddyStrings("ab", "ab")).toEqual(false);
+    expect(buddyStrings("abcaa", "abcbb")).toEqual(false);
   });
 });
